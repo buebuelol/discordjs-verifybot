d8d767f6d19f38019e36e1baa9d32460fff9d263
fjuton <63885722+fjuton@users.noreply.github.com>
2021-06-05 21:24:21 +0200

    tabulatory

--- index.js
+++ index.js
@@ -8,29 +8,34 @@ function checksettings(){
         console.error("[VERIFY BOT] Logs channel ID is not defined!")
         client.destroy()
         return
-        } else if(!config.discord.token) {
-            console.error("[VERIFY BOT] Bot token is not defined!")
-            client.destroy()
-            return
-        } else if(!config.discord.verifychannelid) {
+        
+    } else if(!config.discord.token) {
+        console.error("[VERIFY BOT] Bot token is not defined!")
+        client.destroy()
+        return
+        
+    } else if(!config.discord.verifychannelid) {
         console.error("[VERIFY BOT] Verify channel ID is not defined!")
         client.destroy()
         return
-        } else if(!config.discord.verifiedroleid) {
-            console.error("[VERIFY BOT] Verified role ID is not defined!")
-            client.destroy()
-            return
-        } else if(!config.discord.rolewithpermissionsid) {
-            console.error("[VERIFY BOT] Role with Permissions ID is not defined!")
-            client.destroy()
-            return
-        } else if(!config.discord.serverid) {
-            console.error("[VERIFY BOT] Server ID is not defined!")
-            client.destroy()
-            return
-        } else {
-            console.log("[VERIFY BOT] Bot is online!")
-        }
+        
+    } else if(!config.discord.verifiedroleid) {
+        console.error("[VERIFY BOT] Verified role ID is not defined!")
+        client.destroy()
+        return
+    } else if(!config.discord.rolewithpermissionsid) {
+        console.error("[VERIFY BOT] Role with Permissions ID is not defined!")
+        client.destroy()
+        return
+        
+    } else if(!config.discord.serverid) {
+        console.error("[VERIFY BOT] Server ID is not defined!")
+        client.destroy()
+        return
+        
+    } else {
+        console.log("[VERIFY BOT] Bot is online!")
+    }
 }
 
 client.on("ready", () => {
@@ -41,17 +46,18 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
     if(message.content === config.discord.prefix+"setup") {
-    if(!message.member.roles.cache.some(r => r.id === config.discord.rolewithpermissionsid)) {
-    message.reply('You cannot use this command!')
-    message.delete()
-    return
-    }
-    let verifybutton = new button.MessageButton().setStyle("gray").setLabel("Verify").setID("verifybutton")
-    let verifychannel = client.channels.cache.get(config.discord.verifychannelid)
-    let myembed = new Discord.MessageEmbed().setDescription('``Verify yourself with clicking button below!``').setColor("BLUE");
-    message.delete()
-    verifychannel.send({ button: verifybutton, embed: myembed });
-    console.log("[VERIFY BOT] Setup message has been sended!")
+        if(!message.member.roles.cache.some(r => r.id === config.discord.rolewithpermissionsid)) {
+            message.reply('You cannot use this command!')
+            message.delete()
+            return
+        }
+        
+        let verifybutton = new button.MessageButton().setStyle("gray").setLabel("Verify").setID("verifybutton")
+        let verifychannel = client.channels.cache.get(config.discord.verifychannelid)
+        let myembed = new Discord.MessageEmbed().setDescription('``Verify yourself with clicking button below!``').setColor("BLUE");
+        message.delete()
+        verifychannel.send({ button: verifybutton, embed: myembed });
+        console.log("[VERIFY BOT] Setup message has been sended!")
     }
 })
 
@@ -62,12 +68,11 @@ client.on('clickButton', async (button) => {
     let member = guild.members.cache.get(button.clicker.user.id)
     if(member.roles.cache.some(r => r.id === config.discord.verifiedroleid)) return member.send("```[VERIFY BOT] You are already verified.```")
     if (button.id === 'verifybutton') {
-    button.defer()
-    member.roles.add(config.discord.verifiedroleid)
-    member.send("```[VERIFY BOT] Successfully verified.```")
-    logschannel.send("```"+button.clicker.user.tag + " verified!```")
+        button.defer()
+        member.roles.add(config.discord.verifiedroleid)
+        member.send("```[VERIFY BOT] Successfully verified.```")
+        logschannel.send("```"+button.clicker.user.tag + " verified!```")
     }
-  });
+});
 
 client.login(config.discord.token)
-// bot by bb.#9999
